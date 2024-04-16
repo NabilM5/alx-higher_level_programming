a2aa084e77b94b9b21468b0ca37916668d5cb9d3
NabilM5 <nabil.mouhamech@usmba.ac.ma>
2024-04-16 15:18:14 -0700

    update

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,23 +2,20 @@
 import sys
 import json
 
-# Assuming '5-save_to_json_file.py' and '6-load_from_json_file.py' exist in the same directory
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-args = sys.argv
-filename = "add_item.json"
 
-# Open the file in 'r+' mode instead of 'a+'
-with open(filename, 'r+', encoding="utf-8") as f:
-    # Load existing data if any
-    try:
-        my_list = load_from_json_file(filename)
-    except FileNotFoundError:
-        my_list = []
-    
-    # Append command line arguments individually to the list
-    my_list.extend(args[1:])
-    
-    # Save the updated list to the file
-    save_to_json_file(my_list, filename)
+try:
+    new_list = load_from_json_file('add_item.json')
+except:
+    new_list = []
+
+for i in range(1, len(sys.argv)):
+    new_list.append(sys.argv[i])
+
+try:
+    save_to_json_file(new_list, 'add_item.json')
+except:
+    pass
